2745d2282f7a3e2c23709ca2a1eb2bdb0ff0691a
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-04-27 20:13:19 +0200

    new

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/Home';
 
 export default function App() {
+  const [currentRoute, setRoute] = useState('Home')
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+  });
+  const showPages = () => {
+    switch (currentRoute) {
+      case "Home":
+        return <Home></Home>;
+        break;
+    
+      default:
+        break;
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Task - APP!</Text>
+      {showPages()}
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
