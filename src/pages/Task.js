80312d6b81df6445ae963f8530e31fed19269366
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-05-03 20:39:28 +0200

    adding Task.js Layout

---
+++ src/pages/Task.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import { clickProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
+
+export default (props) => {
+    const styles = StyleSheet.create({
+        tasks: {
+            flex: 1,
+            paddingTop: 20
+        },
+        sectionTitle: {
+            fontSize: 30,
+            color: "#646da1",
+            fontWeight: '700',
+            marginBottom: 20
+        },
+        container: {
+            paddingLeft: 20,
+            paddingRight: 20,
+        },
+        task: {
+            backgroundColor: 'white',
+            borderRadius: 5,
+            padding: 20,
+            marginBottom: 10,
+            shadowColor: '#1b1b1b',
+            shadowOffset: {
+                width:0,
+                height:7
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 6.68
+        },
+        taskTitle: {
+            fontSize: 18,
+            color: '#7c8bc0',
+            fontWeight: '700'
+
+        }
+      });
+      
+      const showTasks = () => {
+          return props.currentTask.map ((task) => {
+            return (
+                <View style={styles.task} key={task.id}>
+                  <Text style={styles.taskTitle}>{task.title}</Text>
+                </View>
+            )
+          })
+      }
+        
+  return (
+    <View style={styles.tasks}>
+        <View style={styles.container}>
+           <Text style={styles.sectionTitle}>Tasks</Text> 
+               {showTasks()}
+        </View>
+    </View>
+  );
+}
